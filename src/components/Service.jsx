0851ef01ed6e0f1bd5b35b8405e82b271f078ea5
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-25 12:24:07 +0100

    Service component responsiveness implemented

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -11,6 +11,7 @@ const Container = styled.div`
     flex-direction: column;
   }
 `
+
 const Left = styled.div`
   width: 50%;
   position: relative;
@@ -18,17 +19,33 @@ const Left = styled.div`
     display: none;
   }
 `
+
 const Image = styled.img`
+  display: ${(props) => props.open && 'none'};
   height: 100%;
   margin-left: 100px;
-  display: ${(props) => props.open && 'none'};
 `
+
+const Video = styled.video`
+  display: ${(props) => !props.open && 'none'};
+  height: 300px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  margin: auto;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
+`
+
 const Right = styled.div`
   width: 50%;
   @media only screen and (max-width: 480px) {
     width: 100%;
   }
 `
+
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
@@ -37,17 +54,21 @@ const Wrapper = styled.div`
     padding: 20px;
   }
 `
+
 const Title = styled.h1``
+
 const Desc = styled.p`
   font-size: 20px;
   margin-top: 20px;
   color: #555;
 `
+
 const CardContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 50px;
 `
+
 const Button = styled.button`
   width: 180px;
   border: none;
@@ -61,21 +82,34 @@ const Button = styled.button`
   display: flex;
   align-items: center;
 `
+
 const Icon = styled.img`
   width: 20px;
   margin-right: 10px;
 `
-const Video = styled.video`
-  display: ${(props) => !props.open && 'none'};
-  height: 300px;
+
+const Modal = styled.div`
+  width: 100vw;
+  height: 100vh;
   position: absolute;
   top: 0;
-  bottom: 0;
-  margin: auto;
-  right: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
 `
-function Service() {
+
+const CloseButton = styled.button`
+  position: absolute;
+  background-color: white;
+  padding: 5px;
+  border: none;
+  border-radius: 5px;
+  right: 5px;
+  top: 30%;
+`
+
+const Service = () => {
   const [open, setOpen] = useState(false)
+  const smallScreen = window.screen.width <= 480 ? true : false
   return (
     <Container>
       <Left>
@@ -92,12 +126,10 @@ function Service() {
         <Wrapper>
           <Title>Simple process to start</Title>
           <Desc>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero eum
-            sint modi? Fugit, animi. Obcaecati voluptates quos eos odio,
-            quibusdam deleniti labore suscipit vel quas enim, ad ea, a
-            consequuntur minima laudantium delectus tenetur officia eaque in
-            fuga? Vitae corporis neque tempore temporibus quae maiores dolor
-            deleniti error cumque doloribus.
+            We provide digital experience services to startups and small
+            businesses to looking for a partner of their digital media, design &
+            development, lead generation and communications requirents. We work
+            with you, not for you. Although we have a great resources
           </Desc>
           <CardContainer>
             <MiniCard />
@@ -110,6 +142,18 @@ function Service() {
           </Button>
         </Wrapper>
       </Right>
+      {smallScreen && open && (
+        <Modal>
+          <Video
+            open={open}
+            autoPlay
+            loop
+            controls
+            src='https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761'
+          />
+          <CloseButton onClick={() => setOpen(false)}>Close</CloseButton>
+        </Modal>
+      )}
     </Container>
   )
 }

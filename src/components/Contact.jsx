db68eb5cd2d939eba796ac1e4d53e4fe24760c85
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-01-25 12:42:30 +0100

    Contact component responsiveness implemented

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,16 +15,25 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `
 
 const FormContainer = styled.div`
   width: 50%;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `
 
 const Title = styled.h1`
   margin: 50px;
   margin-top: 0;
   margin-left: 190px;
+  @media only screen and (max-width: 480px) {
+    margin: 20px;
+  }
 `
 
 const Form = styled.form`
@@ -32,6 +41,9 @@ const Form = styled.form`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `
 
 const LeftForm = styled.div`
@@ -40,6 +52,10 @@ const LeftForm = styled.div`
   flex-direction: column;
   justify-content: space-between;
   margin-right: 20px;
+  @media only screen and (max-width: 480px) {
+    height: 50%;
+    margin-right: 0;
+  }
 `
 
 const RightForm = styled.div`
@@ -47,17 +63,27 @@ const RightForm = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    height: 50%;
+  }
 `
 
 const Input = styled.input`
   width: 200px;
   padding: 20px;
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+  }
 `
 
 const TextArea = styled.textarea`
   width: 200px;
   height: 60%;
   padding: 20px;
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+    margin-top: 20px;
+  }
 `
 
 const Button = styled.button`
@@ -69,6 +95,10 @@ const Button = styled.button`
   border-radius: 10px;
   margin-top: 20px;
   cursor: pointer;
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+    font-size: 14px;
+  }
 `
 
 const AddressContainer = styled.div`
@@ -76,22 +106,35 @@ const AddressContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    margin-top: 20px;
+  }
 `
 
 const AddressItem = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 50px;
+  @media only screen and (max-width: 480px) {
+    margin-bottom: 20px;
+  }
 `
 
 const Icon = styled.img`
   width: 20px;
   margin-right: 20px;
+  @media only screen and (max-width: 480px) {
+    width: 15px;
+  }
 `
 
 const Text = styled.span`
   font-size: 20px;
   margin-right: 15px;
+  @media only screen and (max-width: 480px) {
+    font-size: 14px;
+  }
 `
 
 const Contact = () => {
@@ -117,17 +160,17 @@ const Contact = () => {
         <AddressContainer>
           <AddressItem>
             <Icon src={Map} />
-            <Text>70 Queen Street, Yaba Lagos</Text>
+            <Text>123 Park Avenue St., New York, USA</Text>
           </AddressItem>
           <AddressItem>
             <Icon src={Phone} />
-            <Text>+234 8187 273154</Text>
-            <Text>+234 8023 036608 </Text>
+            <Text>+1 631 1234 5678</Text>
+            <Text>+1 326 1234 5678</Text>
           </AddressItem>
           <AddressItem>
             <Icon src={Send} />
-            <Text>phantmoney2011@gmail.com</Text>
-            <Text>ismail.jamiu19@gmail.com</Text>
+            <Text>contact@lama.dev</Text>
+            <Text>sales@lama.dev</Text>
           </AddressItem>
         </AddressContainer>
       </Wrapper>
